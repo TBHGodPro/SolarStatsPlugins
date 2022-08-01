3a8113d5131d1fbd99039a098ec041c309b4766a
TBHGodPro <tarun.bhaskar@outlook.com>
2022-07-31 22:10:15 -0500

    Added Staff Mods Toggler

---
+++ Plugins/LCStaffMods.js
@@ -0,0 +1,28 @@
+const settingItem = new toolbox.Item(4);
+settingItem.displayName = "§5§lLC §r§fStaff Mods §7(X-RAY)";
+settingItem.lore = ["", "§7Enable §fStaff Mods §7For §5Lunar Client", "", `§7Current: §${toolbox.getConfigSync().modules.staffMods ? "aEnabled" : "cDisabled"}`];
+
+const module = new toolbox.PlayerModule("LC Staff Mods (X-RAY)", "Enable Staff Mods For Lunar Client", settingItem, "staffMods");
+
+var oldConfig = null;
+var config = toolbox.getConfigSync();
+
+module.customCode = () => {
+	setInterval(() => {
+		oldConfig = config;
+		config = toolbox.getConfigSync();
+		if (JSON.stringify(oldConfig) != JSON.stringify(config)) {
+			if (config.modules.staffMods) player.lcPlayer?.setStaffModState("XRAY", true);
+			else player.lcPlayer?.setStaffModState("XRAY", false);
+		}
+	}, 500);
+};
+
+registerPlugin({
+	name: "LC Staff Mods (X-RAY)",
+	description: "Enable Staff Mods For Lunar Client",
+	version: "1.0.0",
+	author: "TBHGodPro"
+});
+
+registerPlayerModule(module);
