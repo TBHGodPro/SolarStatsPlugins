3ddd5a207218c8374977eb44f07c141dbc3183e1
TBHGodPro <tarun.bhaskar@outlook.com>
2022-08-01 20:24:51 -0500

    Added ThanksWatchdog

---
+++ Plugins/ThanksWatchdog.js
@@ -0,0 +1,19 @@
+player.proxy.on("incoming", (data, meta, toClient, toServer) => {
+	if (meta.name != "chat") return;
+	var message = JSON.parse(data.message);
+	message.extra ??= [];
+	message = [message.text, ...message.extra.map(i => i.text)].join("");
+
+	if (message.match(/watchdog/gi)) {
+		toServer.write("chat", {
+			message: `Thanks Watchdog${"!".repeat(Math.ceil(Math.random() * 3))}`
+		});
+	}
+});
+
+registerPlugin({
+	name: "Thanks Watchdog!",
+	description: "Thank Watchdog For Absolutely Nothing :P",
+	version: "1.0.0",
+	author: "TBHGodPro"
+});
