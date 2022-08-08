9460e3ece282eeeb2d96d7d0d1b4994c32dac652
TBHGodPro <tarun.bhaskar@outlook.com>
2022-08-08 09:53:12 -0500

    small changes

--- Plugins/skyblockAutoHarp.js
+++ Plugins/skyblockAutoHarp.js
@@ -1,9 +1,8 @@
-const { PlayerModule, Item, getConfigSync } = toolbox;
-var enabled = getConfigSync().modules.skyblockAutoHarp;
+const { PlayerModule, Item, getConfig, getConfigSync } = toolbox;
 
 const settingItem = new Item(261);
 settingItem.displayName = "§rSkyblock Auto-Harp";
-settingItem.lore = ["", "§7Automatically Do Harp Songs (MACRO)", "§4§lWARNING: §r§cREQUIRES DECENT PING", "", `§7Current: §${enabled ? "aEnabled" : "cDisabled"}`];
+settingItem.lore = ["", "§7Automatically Do Harp Songs (MACRO)", "§4§lWARNING: §r§cREQUIRES DECENT PING", "", `§7Current: §${getConfigSync().modules.skyblockAutoHarp ? "aEnabled" : "cDisabled"}`];
 
 const module = new PlayerModule("Skyblock Auto-Harps", "Automatically Do Harp Songs (MACRO)", settingItem, "skyblockAutoHarp");
 
@@ -11,7 +10,7 @@ var windowId = null;
 var nextAction = null;
 
 async function list(data, meta, toClient, toServer) {
-	if (!enabled) return;
+	if (!(await getConfig()).modules.skyblockAutoHarp) return;
 	if (meta.name == "window_click") {
 		nextAction = data.action + 1;
 	}
@@ -57,15 +56,11 @@ async function list(data, meta, toClient, toServer) {
 player.proxy.on("incoming", list);
 player.proxy.on("outgoing", list);
 
-module.onConfigChange = e => {
-	enabled = e;
-};
-
 registerPlayerModule(module);
 
 registerPlugin({
 	name: "Skyblock Auto-Harp",
 	description: "Automatically Do Harp Songs (MACRO) | `/ss`",
-	version: "1.0.1",
+	version: "1.0.2",
 	author: "TBHGodPro"
 });
